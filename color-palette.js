bf790974756101617078932d007d0a07a001ea4e
Charles Wooley <cwooley89@gmail.com>
2017-07-31 15:16:22 -0400

    Added more colors

--- color-palette.js
+++ color-palette.js
@@ -1,5 +1,5 @@
-let colors = ['rgb(255,0,0)', 'rgb(255,255,255)', 'rgb(0,0,255)' ];
-
+// let colors = ['rgb(255,0,0)', 'rgb(255,255,255)', 'rgb(0,0,255)', 'rgb(255, 187, 0)',  'rgb(118,42,145)', 'rgb(0, 255, 0)', 'rgb(255, 129, 0)', 'rgb(139,69,19)' ];
+  let colors = ['black', 'maroon', 'green', 'navy', 'white', 'red', 'lime', 'blue', 'gray', 'purple', 'fuchsia', 'cyan', 'olive', 'yellow', 'magenta', 'teal' ]
 class ColorPalette {
 
   constructor(activeColor){
@@ -13,7 +13,7 @@ class ColorPalette {
   render(){
 
     let HTML = colors.map((color)=>{
-      return `<div class="color-picker" data-color="${color}" style="background-color: ${color}; cursor:pointer; width: 25px; height: 25px;">
+      return `<div class="color-picker" data-color="${color}" style="background-color: ${color}; cursor:pointer; width: 25px; height: 25px; border-radius: 50%;">
       </div>`
     }).join(' ')
     $('#palette-goes-here').html(HTML)
